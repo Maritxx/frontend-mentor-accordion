7356b16d62d3aee64dd124616b019fd2c3a1cdfd
Maritxx <maritdevries1@live.nl>
2022-07-03 22:11:19 +0200

    Initial setup

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+const faqButtons = document.getElementsByClassName(".faqQuestion");
 
+faqButtons.forEach((FaqQuestion) => {
+	faqQuestion.addEventListener("click", () => {
+		if (faqQuestion.classList.contains("active")) {
+			faqQuestion.classList.remove("active");
+		} else {
+			const faqQuestionsWithActive = document.getElementsByClassName(".active");
+			faqQuestionsWithActive.forEach((faqQuestionWithActive) => {
+				faqQuestionWithActive.classList.remove("active");
+			});
+			faqQuestion.classList.add("active");
+		}
+	});
+});
