dc265bfff152ac4face4d4a459575d8f8460601f
Maritxx <maritdevries1@live.nl>
2022-07-03 22:36:25 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const faqButtons = document.getElementsByClassName(".faqQuestion");
+const faqQuestions = document.querySelectorAll(".faqQuestion");
 
-faqButtons.forEach((FaqQuestion) => {
+faqQuestions.forEach((faqQuestion) => {
 	faqQuestion.addEventListener("click", () => {
 		if (faqQuestion.classList.contains("active")) {
 			faqQuestion.classList.remove("active");
 		} else {
-			const faqQuestionsWithActive = document.getElementsByClassName(".active");
+			const faqQuestionsWithActive = document.querySelectorAll(".active");
 			faqQuestionsWithActive.forEach((faqQuestionWithActive) => {
 				faqQuestionWithActive.classList.remove("active");
 			});
